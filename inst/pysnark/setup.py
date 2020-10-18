5c78e770b1e9f92d032e1a4c32fa59751b61039f
Alec Wong <aw685@cornell.edu>
2020-10-18 12:51:04 -0400

    add to setup.py for pip installation

---
+++ inst/pysnark/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='pysnark',
+    version='0.1.0',
+    description='Snarky text in python',
+    author='Alec Wong',
+    author_email='aw685@cornell.edu',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5'
+)
